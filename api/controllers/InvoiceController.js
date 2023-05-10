9a8a8d92e51fe095847fb2e1a19a6d24ae4709bd
MonyDev <rysarakmony1666@gmail.com>
2023-05-10 23:59:29 +0700

    new

---
+++ api/controllers/InvoiceController.js
@@ -0,0 +1,126 @@
+const Invoice = require("../models/InvoiceModel");
+const TelegramBot = require("node-telegram-bot-api");
+require("dotenv").config();
+const send_message = (message) => {
+  try {
+    const token = process.env.TELEGRAM_BOT_TOKEN;
+    const chatId = process.env.CHAT_ID;
+    const bot = new TelegramBot(token, { polling: true });
+    bot.sendMessage(chatId, message);
+  } catch (err) {}
+};
+
+module.exports.create = async (req, res, next) => {
+  try {
+    let invoice = new Invoice(
+      req.body.payment_id,
+      req.body.amount,
+      req.body.remain
+    );
+
+    [invoice] = await invoice.save();
+    const [invoice_number] = await Invoice.generateInvoiceNumber(
+      invoice.insertId
+    );
+    res.send({ id: invoice.insertId });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.saleInvoice = async (req, res, next) => {
+  try {
+    const [invoice] = await Invoice.saleInvoice(req.params.id);
+
+    // ========= send message to telegram ==========
+
+    let text = "";
+    let saller = "";
+    let total = 0;
+    let payment = "";
+    let amount = 0;
+    let moneyChange = 0;
+    let invoiceNumber = "";
+
+    invoice[0].map((item) => {
+      total += item.subtotal;
+      invoiceNumber = item.invoice_number;
+      saller =
+        "Saller: " +
+        item.username +
+        "\nInvoice number: " +
+        invoiceNumber +
+        "\nDate: " +
+        item.sale_date +
+        "\n\n";
+
+      payment = "Payment By: " + item.payment_type;
+      amount = "\nAmount: $" + item.amount + ".00";
+      moneyChange = "\nChange: $" + item.money_change + ".00";
+      text +=
+        item.product_name +
+        " " +
+        "\nQty: " +
+        item.qty +
+        " " +
+        "\nPrice: $" +
+        item.subtotal +
+        ".00" +
+        "\n" +
+        "---------------------\n";
+    });
+    if (text !== "" && saller !== "" && total !== 0) {
+      send_message(
+        saller +
+          text +
+          "Total: $" +
+          total +
+          ".00\n" +
+          payment +
+          amount +
+          moneyChange
+      );
+    }
+    // ========= end of message =============
+
+    // ====== send invoice to user ==========
+    //console.log(invoice);
+    res.send(invoice);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.view_invoice = async (req, res, next) => {
+  try {
+    const [invoice] = await Invoice.saleInvoice(req.params.id);
+    res.send(invoice);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.update_invoice = async (req, res, next) => {
+  try {
+    //console.log(req.body);
+    const id = req.params.id;
+    const amount = req.body.amount;
+    const money_change = req.body.remain;
+    const payment_id = req.body.payment_id;
+
+    const [result] = await Invoice.updateInvoice(
+      id,
+      payment_id,
+      amount,
+      money_change
+    );
+
+    if (result.affectedRows > 0) {
+      res.send({ message: "Updated", success: true });
+    } else {
+      res.send({ message: "Failed", success: true });
+    }
+  } catch (err) {
+    next(err);
+  }
+};
---
+++ api/models/model.brand.js
@@ -0,0 +1,50 @@
+const db = require("../config/db");
+
+class Brnad {
+  constructor(brandName, desc) {
+    this.brandName = brandName;
+    this.desc = desc;
+  }
+
+  save() {
+    const sql = "INSERT INTO tblBrands(`brandName`,`desc`) VALUES(?,?)";
+    return db.execute(sql, [this.brandName, this.desc]);
+  }
+
+  static updateById(brandName, desc, id) {
+    const sql = "UPDATE tblBrands SET `brandName`=?,`desc`=? WHERE `id` = ?";
+    return db.query(sql, [brandName, desc, id]);
+  }
+
+  static deleteById(id) {
+    const sql = "DELETE FROM tblBrands WHERE id = ?";
+    return db.execute(sql, [id]);
+  }
+
+  static findByName(brandName) {
+    const sql = "SELECT *FROM tblBrands WHERE brandName=?";
+    return db.execute(sql, [brandName]);
+  }
+
+  static updateDuplicate(id, brandName) {
+    const sql = "SELECT *FROM tblBrands WHERE NOT id=? AND brandName=?";
+    return db.execute(sql, [id, brandName]);
+  }
+
+  static findById(id) {
+    const sql = "SELECT *FROM tblBrands WHERE id=?";
+    return db.execute(sql, [id]);
+  }
+
+  static findAll(limit, page, keyword) {
+    const sql = "CALL SP_Brand(?,?,?)";
+    return db.execute(sql, [limit, page, keyword]);
+  }
+
+  static getRows(search) {
+    const sql = "CALL SP_GetRowsBrand(?)";
+    return db.execute(sql, [search]);
+  }
+}
+
+module.exports = Brnad;
